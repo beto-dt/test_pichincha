e65e3ed8d3e706cbcac00d46b5ba72ff47b269b7
beto-dt-21 <luis.delatorre@kinanalytics.com>
2022-09-02 22:15:43 -0500

    feat:Create file routes dynamic

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const models = {
+    personsModel: require('./persons'),
+    usersModel: require('./users'),
+    accountsModel: require('./accounts'),
+    transactionsModel: require('./transactions')
+}
+
+module.exports = models;
