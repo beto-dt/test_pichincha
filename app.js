170cd70c80028fca15851067c58fee74ee45cf3d
beto-dt-21 <luis.delatorre@kinanalytics.com>
2022-09-02 22:15:34 -0500

    feat:Create file routes dynamic

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+//TODO https://localhost/api/___________
+app.use("/api/v1",require('./routes'));
+
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
