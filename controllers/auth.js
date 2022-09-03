07fc9b6d042e1833dd7442a61f229374f39980e0
beto-dt-21 <luis.delatorre@kinanalytics.com>
2022-09-03 17:12:13 -0500

    feat:auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,4 +56,36 @@ const loginUser = async(req, res) => {
     }
 }
 
-module.exports = { registerUser, loginUser }
+/**
+ * Update a register
+ * @param {*} req 
+ * @param {*} res 
+ */
+ const updateLogin = async (req, res) => {
+    try{
+        const body = req.body;
+        const id = req.params.id;
+        const data = await usersModel.update(body,{where:{ id : id}})
+        res.send({data})
+    }catch(e){
+        handleHttpError(res,'ERROR_UPDATE_ITEMS',e)
+    }
+};
+
+/**
+ * Delete a register
+ * @param {*} req 
+ * @param {*} res 
+ */
+const deleteLogin = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const data = await usersModel.destroy({where:{ id : id}})
+        res.send({data})
+    }catch(e){
+        handleHttpError(res,'ERROR_DELETE_ITEMS',e)
+
+    }
+};
+
+module.exports = { registerUser, loginUser,updateLogin ,deleteLogin }
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/auth');
+const { registerUser, loginUser, updateLogin, deleteLogin } = require('../controllers/auth');
 const router = express.Router();
 
 //TODO http://localhost:3001/api/v1/auth/login
 //TODO http://localhost:3001/api/v1/auth/register
 router.post("/register", registerUser )
 router.post("/login", loginUser )
+router.put("/:id", updateLogin )
+router.delete("/:id", deleteLogin )
 
 
 module.exports = router;
